a43a82f75836a77b8e80c96c3a26b03a381abbc9
Simon Dell <simon@expressionworks.co.uk>
2016-06-21 17:06:22 +0100

    More prompt tweaks. Cutting out iTerm2 config as it was annoying me.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,3 @@
 # PROMPTS
-export PROMPT="-- in %~ -------- at %D %*%E"$'\n'%#%_"> "
+export PROMPT="in %~ ---------------------------------- at %* on %D%E"$'\n'%#%_"> "
 #export RPROMPT="%D %*" # yy-mm-dd 24:mm:ss
