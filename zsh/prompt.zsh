d4924b7b37a3e106a2f7ab1603f7c90c9a8b6cdd
Simon Dell <simon@expressionworks.co.uk>
2016-06-24 11:39:30 +0100

    Terser prompt. Project-specific aliases (ignored).

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,6 @@
 # PROMPTS
-export PROMPT="in %~ ---------------------------------- at %* on %D%E"$'\n'%#%_"> "
+# %#%F{yellow}%n%F{cyan}@%m %F{default}
+export PROMPT='
+%!| %F{yello}%~%F{default} | %* / %D%E
+✪ '
 #export RPROMPT="%D %*" # yy-mm-dd 24:mm:ss
