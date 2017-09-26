7e4af7f913aceeb0883922acf97fb3a1eda83998
Simon Dell <simon@expression.works>
2017-09-26 15:15:35 +0100

    Prompt tweaks

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,6 @@
 # PROMPTS
 # %#%F{yellow}%n%F{cyan}@%m %F{default}
 export PROMPT='
-%!| %F{yello}%~%F{default} | %* / %D%E
+%!| %F{yello}%~%F{default} | %* %D%E
 ☿ '
 #export RPROMPT="%D %*" # yy-mm-dd 24:mm:ss
